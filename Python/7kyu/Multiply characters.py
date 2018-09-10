bb4870a3a7757c9d1150a5d25ef79e9deeac7ba4
mlangs <39197791+mlangs@users.noreply.github.com>
2018-09-10 14:45:58 +0200

    Add files via upload

---
+++ Python/7kyu/Multiply characters.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/52e9aa89b5acdd26d3000127
+#
+# spam(1)  ==> "hue"
+# spam(6)  ==> "huehuehuehuehuehue"
+# spam(14) ==> "huehuehuehuehuehuehuehuehuehuehuehuehuehue"
+
+def spam(number):
+    return 'hue'*number
