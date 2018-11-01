0af502fb2bbb751ee910b04230fee988fdd5d1a4
mlangs <39197791+mlangs@users.noreply.github.com>
2018-11-01 16:46:00 +0100

    added 3 more files
    added 'Abbreviate a Two Word Name.py', 'Convert a Number to a String.py' and 'You only need one - Beginner.py'

---
+++ Python/8kyu/Abbreviate a Two Word Name.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+#
+# Write a function to convert a name into initials. This kata strictly takes
+# two words with one space in between them.
+#
+# The output should be two capital letters with a dot seperating them.
+#
+# It should look like this:
+#
+# Sam Harris => S.H
+#
+# Patrick Feeney => P.F
+
+
+def abbrevName(name):
+    name = name.split(' ')
+
+    initials = []
+    for n in name:
+        initials.append(n[0].upper())
+
+    return ".".join(initials)
